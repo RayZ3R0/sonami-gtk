0edc72497e0751c29c7a8ec1ed0b51f76c534d4b
NilaTheDragon <nilathedragon@pm.me>
2025-12-23 00:18:59 +0100

    Add icons
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

---
+++ internal/icons/icons.go
@@ -0,0 +1,22 @@
+package icons
+
+import (
+	_ "embed"
+	"log"
+
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
+	"github.com/diamondburned/gotk4/pkg/glib/v2"
+)
+
+//go:generate glib-compile-resources icons.gresource.xml
+
+//go:embed icons.gresource
+var Resources []byte
+
+func init() {
+	resources, err := gio.NewResourceFromData(glib.NewBytesWithGo(Resources))
+	if err != nil {
+		log.Panicln("Failed to create resources: ", err)
+	}
+	gio.ResourcesRegister(resources)
+}
