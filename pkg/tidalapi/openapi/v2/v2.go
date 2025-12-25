4ba8c5ca6b9764a617239372ff70b1c9f882c03e
NilaTheDragon <nilathedragon@pm.me>
2025-12-25 21:15:57 +0100

    Implement Album OpenAPI
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- pkg/tidalapi/openapi/v2/v2.go
+++ pkg/tidalapi/openapi/v2/v2.go
@@ -2,17 +2,20 @@ package v2
 
 import (
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi/internal"
+	"codeberg.org/dergs/tidalwave/pkg/tidalapi/openapi/v2/albums"
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi/openapi/v2/playlists"
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi/openapi/v2/tracks"
 )
 
 type V2 struct {
+	Albums    *albums.Albums
 	Playlists *playlists.Playlists
 	Tracks    *tracks.Tracks
 }
 
 func New(client *internal.Client) *V2 {
 	return &V2{
+		Albums:    albums.New(client),
 		Playlists: playlists.New(client),
 		Tracks:    tracks.New(client),
 	}
