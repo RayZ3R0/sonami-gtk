46c36a69d6ee73aa45dc0b051bb1c7c1a27ff21b
drafolin <derg@drafolin.ch>
2025-12-27 15:29:33 +0100

    Show track version in title

--- internal/ui/components/tracklist/column_title.go
+++ internal/ui/components/tracklist/column_title.go
@@ -1,6 +1,8 @@
 package tracklist
 
 import (
+	"fmt"
+
 	"codeberg.org/dergs/tidalwave/pkg/gui"
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi/models/openapi"
 	v2 "codeberg.org/dergs/tidalwave/pkg/tidalapi/models/v2"
@@ -15,7 +17,11 @@ func titleColumn(title string, grid *gtk.Grid, row int, column int) int {
 }
 
 func TitleColumn(track *openapi.Track, grid *gtk.Grid, row int, column int) int {
-	return titleColumn(track.Data.Attributes.Title, grid, row, column)
+	title := track.Data.Attributes.Title
+	if version := track.Data.Attributes.Version; version != nil && version != "" {
+		title = fmt.Sprintf("%s (%s)", title, version)
+	}
+	return titleColumn(title, grid, row, column)
 }
 
 func LegacyTitleColumn(track *v2.TrackItemData, grid *gtk.Grid, row int, column int) int {
