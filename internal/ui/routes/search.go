301469284a54ec7ce38cb190f86401ba81e7f462
drafolin <derg@drafolin.ch>
2025-12-27 16:28:17 +0100

    Add Enter key handling on search

--- internal/ui/routes/search.go
+++ internal/ui/routes/search.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"codeberg.org/dergs/tidalwave/internal/router"
 	"codeberg.org/dergs/tidalwave/internal/ui/routes/search"
 	"codeberg.org/dergs/tidalwave/internal/ui/signals"
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi"
+	"github.com/diamondburned/gotk4/pkg/gdk/v4"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"github.com/go-gst/go-glib/glib"
 	"github.com/infinytum/injector"
@@ -23,8 +25,28 @@ func init() {
 		searchBar.SetHExpand(true)
 		searchBar.SetMarginEnd(40)
 		searchBar.SetPlaceholderText("E.g. Fox Stevenson...")
-		searchBar.ConnectSearchChanged(OnSearch(searchBar, scrolledWindow))
+		onTypeHandler := searchBar.ConnectSearchChanged(OnSearch(searchBar, scrolledWindow))
 		searchBar.SetSearchDelay(1000)
+
+		keyController := gtk.NewEventControllerKey()
+		keyController.SetPropagationPhase(gtk.PhaseCapture)
+		keyController.ConnectKeyPressed(func(keyval, keycode uint, state gdk.ModifierType) (ok bool) {
+			if keyval == gdk.KEY_Return {
+				OnSearch(searchBar, scrolledWindow)()
+
+				// Block the typing handler for 1 second
+				searchBar.HandlerBlock(onTypeHandler)
+				time.AfterFunc(1*time.Second, func() {
+					searchBar.HandlerUnblock(onTypeHandler)
+				})
+
+				return true // Stop further propagation
+			}
+
+			return false // Allow further propagation
+		})
+		searchBar.AddController(keyController)
+
 		router.NavigationComplete.On(func(response *router.Response) bool {
 			searchBar.GrabFocus()
 			return signals.Continue
