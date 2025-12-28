8b80e36af13027965f71cd1b46625418a17d20dc
NilaTheDragon <nilathedragon@pm.me>
2025-12-28 14:41:47 +0100

    ActionName, ActionTargetValue & IconName for Button
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- pkg/schwifty/button.go
+++ pkg/schwifty/button.go
@@ -1,21 +1,48 @@
 package schwifty
 
-import "github.com/jwijenbergh/puregotk/v4/gtk"
+import (
+	"github.com/jwijenbergh/puregotk/v4/glib"
+	"github.com/jwijenbergh/puregotk/v4/gtk"
+)
 
 //go:generate go run codeberg.org/dergs/tidalwave/pkg/schwifty/gen Button *gtk.Button
 
+func (f Button) ActionName(actionName string) Button {
+	return func() *gtk.Button {
+		button := f()
+		button.SetActionName(actionName)
+		return button
+	}
+}
+
+func (f Button) ActionTargetValue(targetValue *glib.Variant) Button {
+	return func() *gtk.Button {
+		button := f()
+		button.SetActionTargetValue(targetValue)
+		return button
+	}
+}
+
 func (f Button) Child(widget any) Button {
 	return func() *gtk.Button {
-		clamp := f()
-		clamp.SetChild(ResolveWidget(widget))
-		return clamp
+		button := f()
+		button.SetChild(ResolveWidget(widget))
+		return button
 	}
 }
 
 func (f Button) ConnectClicked(cb func(gtk.Button)) Button {
 	return func() *gtk.Button {
-		clamp := f()
-		clamp.ConnectClicked(&cb)
-		return clamp
+		button := f()
+		button.ConnectClicked(&cb)
+		return button
+	}
+}
+
+func (f Button) IconName(iconName string) Button {
+	return func() *gtk.Button {
+		button := f()
+		button.SetIconName(iconName)
+		return button
 	}
 }
