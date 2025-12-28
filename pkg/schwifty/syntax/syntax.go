8df857972e80ba0c3d876625f737f5e264620ed1
NilaTheDragon <nilathedragon@pm.me>
2025-12-29 00:42:12 +0100

    Re-use destroy handler for unreferencing
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- pkg/schwifty/syntax/syntax.go
+++ pkg/schwifty/syntax/syntax.go
@@ -4,12 +4,16 @@ import (
 	"log/slog"
 	"runtime"
 
-	"codeberg.org/dergs/tidalwave/internal/g"
 	"codeberg.org/dergs/tidalwave/pkg/schwifty/tracking"
 	"github.com/jwijenbergh/puregotk/v4/gtk"
 )
 
 var logger = slog.With("library", "schwifty")
+
+var onDestroy = func(a gtk.Widget) {
+	logger.Debug("object was destroyed by GTK", "ptr", a.GoPointer())
+	tracking.Untrack(a.GoPointer())
+}
 
 type unrefable interface {
 	ConnectDestroy(cb *func(gtk.Widget)) uint32
@@ -21,13 +25,11 @@ func finalize[T unrefable](tag string, widgetToBeFinalized T) {
 	ptr := widgetToBeFinalized.GoPointer()
 	tracking.Track(ptr, tag)
 	logger.Debug("now managing cleanup for object", "ptr", ptr, "tag", tag)
-	widgetToBeFinalized.ConnectDestroy(g.Ptr(func(a gtk.Widget) {
-		logger.Debug("object was destroyed by GTK", "ptr", a.GoPointer(), "tag", tag)
-		tracking.Untrack(ptr)
-	}))
+	widgetToBeFinalized.ConnectDestroy(&onDestroy)
 	runtime.SetFinalizer(widgetToBeFinalized, func(finalizedWidget T) {
 		logger.Debug("releasing reference on schwifty-managed object, GTK may still hold a reference to this object", "tag", tag, "ptr", finalizedWidget.GoPointer())
 		finalizedWidget.Unref()
+		tracking.TrackGC(ptr)
 	})
 }
 
