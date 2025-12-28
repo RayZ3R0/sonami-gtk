c81a8700eab2bd25bc9d9020674b0b36e0ea186a
NilaTheDragon <nilathedragon@pm.me>
2025-12-29 00:41:51 +0100

    Make tracking thread-safe
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- pkg/schwifty/tracking/tracking.go
+++ pkg/schwifty/tracking/tracking.go
@@ -1,13 +1,18 @@
 package tracking
 
+import "sync"
+
 type TrackedWidget struct {
 	IsReferencedByGo bool
 	Type             string
 }
 
 var trackedWidgets map[uintptr]*TrackedWidget = make(map[uintptr]*TrackedWidget)
+var lock sync.Mutex = sync.Mutex{}
 
 func Track(ptr uintptr, widgetType string) {
+	lock.Lock()
+	defer lock.Unlock()
 	trackedWidgets[ptr] = &TrackedWidget{
 		IsReferencedByGo: true,
 		Type:             widgetType,
@@ -15,16 +20,22 @@ func Track(ptr uintptr, widgetType string) {
 }
 
 func TrackGC(ptr uintptr) {
+	lock.Lock()
+	defer lock.Unlock()
 	if val, ok := trackedWidgets[ptr]; ok {
 		val.IsReferencedByGo = false
 	}
 }
 
 func Untrack(ptr uintptr) {
+	lock.Lock()
+	defer lock.Unlock()
 	delete(trackedWidgets, ptr)
 }
 
 func Alive() []*TrackedWidget {
+	lock.Lock()
+	defer lock.Unlock()
 	var alive []*TrackedWidget
 	for _, widget := range trackedWidgets {
 		alive = append(alive, widget)
