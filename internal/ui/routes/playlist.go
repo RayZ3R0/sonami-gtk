113e8b1b6112fcdc29caa86fb1a3a63e37c0ad32
NilaTheDragon <nilathedragon@pm.me>
2025-12-30 01:38:22 +0100

    Re-Implement Playlist view
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

---
+++ internal/ui/routes/playlist.go
@@ -0,0 +1,116 @@
+package routes
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"codeberg.org/dergs/tidalwave/internal/resources"
+	"codeberg.org/dergs/tidalwave/internal/router"
+	"codeberg.org/dergs/tidalwave/internal/ui/components/tracklist"
+	"codeberg.org/dergs/tidalwave/pkg/schwifty"
+	. "codeberg.org/dergs/tidalwave/pkg/schwifty/syntax"
+	"codeberg.org/dergs/tidalwave/pkg/tidalapi"
+	"codeberg.org/dergs/tidalwave/pkg/tidalapi/models/openapi"
+	"codeberg.org/dergs/tidalwave/pkg/utils/imgutil"
+	"github.com/infinytum/injector"
+	"github.com/jwijenbergh/puregotk/v4/gtk"
+)
+
+func init() {
+	router.Register("playlist", Playlist)
+}
+
+func Playlist(params router.Params) *router.Response {
+	playlistUUID, ok := params["id"].(string)
+	if !ok {
+		return router.FromError("Playlist", errors.New("invalid playlist ID"))
+	}
+
+	tidal := injector.MustInject[*tidalapi.TidalAPI]()
+
+	playlist, err := tidal.OpenAPI.V2.Playlists.Playlist(context.Background(), playlistUUID, "coverArt", "ownerProfiles")
+	if err != nil {
+		return router.FromError("Playlist", err)
+	}
+
+	// TODO: Handle pagination with scroll events
+	items, err := tidal.OpenAPI.V2.Playlists.Items(context.Background(), playlistUUID, "", "items", "items.artists", "items.albums", "items.albums.coverArt")
+	if err != nil {
+		return router.FromError("Playlist", err)
+	}
+
+	creator := "TIDAL"
+	for _, artist := range playlist.Included.PlainArtists(playlist.Data.Relationships.OwnerProfiles.Data...) {
+		creator = artist.Attributes.Name
+		break
+	}
+
+	coverUrl := ""
+	for _, artwork := range playlist.Included.PlainArtworks(playlist.Data.Relationships.CoverArt.Data...) {
+		coverUrl = artwork.Attributes.Files.AtLeast(320).Href
+	}
+
+	list := tracklist.NewTrackList(
+		"",
+		tracklist.CoverColumn,
+		tracklist.TitleAlbumColumn,
+		tracklist.ArtistsColumn,
+		tracklist.DurationColumn,
+		tracklist.ButtonColumn,
+		tracklist.ControlsColumn,
+	)
+
+	for _, track := range items.Included.Tracks(items.Data...) {
+		list.AddTrack(&track)
+	}
+
+	var playlistMetadata schwifty.Label
+	if playlist.Data.Attributes.PlaylistType != openapi.PlaylistTypeMix {
+		playlistMetadata = Label(fmt.Sprintf("%d Tracks (%s)", playlist.Data.Attributes.NumberOfItems, tidalapi.FormatDuration(int(playlist.Data.Attributes.Duration.Seconds()))))
+	} else {
+		playlistMetadata = Label("Personal Mix")
+	}
+
+	return &router.Response{
+		PageTitle: playlist.Data.Attributes.Name,
+		View: VStack(
+			HStack(
+				AspectFrame(
+					Image().
+						PixelSize(146).
+						FromPaintable(resources.MissingAlbum()).
+						ConnectConstruct(func(i *gtk.Image) {
+							if coverUrl != "" {
+								injector.MustInject[*imgutil.ImgUtil]().LoadIntoImage(coverUrl, i)
+							}
+						}),
+				).CornerRadius(10).Overflow(gtk.OverflowHiddenValue),
+				VStack(
+					Label(playlist.Data.Attributes.Name).
+						FontSize(18).
+						FontWeight(700).
+						HAlign(gtk.AlignStartValue),
+					Label(creator).
+						FontSize(16).
+						FontWeight(500).
+						HAlign(gtk.AlignStartValue),
+					Label(playlist.Data.Attributes.CreatedAt.Format("2006")).
+						FontSize(16).
+						FontWeight(500).
+						HAlign(gtk.AlignStartValue),
+					playlistMetadata.
+						FontSize(14).
+						FontWeight(600).
+						HAlign(gtk.AlignStartValue).
+						MarginTop(10),
+				).MarginStart(20).VAlign(gtk.AlignCenterValue),
+			),
+			ScrolledWindow().
+				Child(list).
+				Policy(gtk.PolicyNeverValue, gtk.PolicyAutomaticValue).
+				VExpand(true).
+				MarginTop(20),
+		).HMargin(40).VMargin(20),
+	}
+}
