34641b0ac527e7b503844babfe8f5ebfc1810217
NilaTheDragon <nilathedragon@pm.me>
2025-12-31 03:35:55 +0100

    Add action to add-to-queue button
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- internal/ui/components/tracklist/column_controls.go
+++ internal/ui/components/tracklist/column_controls.go
@@ -1,13 +1,16 @@
 package tracklist
 
 import (
+	"strconv"
+
 	. "codeberg.org/dergs/tidalwave/pkg/schwifty/syntax"
 	"codeberg.org/dergs/tidalwave/pkg/tidalapi/models/openapi"
 	v2 "codeberg.org/dergs/tidalwave/pkg/tidalapi/models/v2"
+	"github.com/jwijenbergh/puregotk/v4/glib"
 	"github.com/jwijenbergh/puregotk/v4/gtk"
 )
 
-func controlsColumn(grid *gtk.Grid, row int, column int) int {
+func controlsColumn(trackId string, grid *gtk.Grid, row int, column int) int {
 	grid.Attach(
 		HStack(
 			Button().
@@ -19,6 +22,8 @@ func controlsColumn(grid *gtk.Grid, row int, column int) int {
 				IconName("plus-symbolic").
 				HAlign(gtk.AlignCenterValue).
 				VAlign(gtk.AlignCenterValue).
+				ActionName("win.player.queue-track").
+				ActionTargetValue(glib.NewVariantString(trackId)).
 				CSS(`button:not(:hover) { background-color: transparent; }`),
 		).
 			Margin(10).
@@ -33,9 +38,9 @@ func controlsColumn(grid *gtk.Grid, row int, column int) int {
 }
 
 func ControlsColumn(track *openapi.Track, grid *gtk.Grid, row int, column int) int {
-	return controlsColumn(grid, row, column)
+	return controlsColumn(track.Data.ID, grid, row, column)
 }
 
 func LegacyControlsColumn(track *v2.TrackItemData, grid *gtk.Grid, row int, column int) int {
-	return controlsColumn(grid, row, column)
+	return controlsColumn(strconv.Itoa(track.ID), grid, row, column)
 }
