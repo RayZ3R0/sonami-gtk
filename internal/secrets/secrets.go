a54a9c3d997de344ca1a034859e5ec4af3fb7e08
NilaTheDragon <nilathedragon@pm.me>
2026-01-02 19:34:44 +0100

    Add UserID extractor to secrets
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- internal/secrets/secrets.go
+++ internal/secrets/secrets.go
@@ -1,6 +1,13 @@
 package secrets
 
-import "github.com/zalando/go-keyring"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"strconv"
+	"strings"
+
+	"github.com/zalando/go-keyring"
+)
 
 const (
 	service         = "org.codeberg.dergs.tidalwave"
@@ -26,4 +33,34 @@ func SetRefreshToken(token string) error {
 
 func DeleteRefreshToken() error {
 	return keyring.Delete(service, refreshTokenKey)
+}
+
+func UserID() string {
+	if !HasRefreshToken() {
+		return ""
+	}
+
+	token := GetRefreshToken()
+
+	// Split JWT into parts (header.payload.signature)
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return ""
+	}
+
+	// Decode the payload (second part)
+	payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return ""
+	}
+
+	// Parse JSON payload
+	var claims struct {
+		UID int `json:"uid"`
+	}
+	if err := json.Unmarshal(payload, &claims); err != nil {
+		return ""
+	}
+
+	return strconv.Itoa(claims.UID)
 }
