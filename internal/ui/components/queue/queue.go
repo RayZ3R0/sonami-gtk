fc144c21fbffb6c3ee1e20fad1ae85a53f5b2d4d
NilaTheDragon <nilathedragon@pm.me>
2026-01-14 19:37:59 +0100

    Fix queue button being misaligned
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- internal/ui/components/queue/queue.go
+++ internal/ui/components/queue/queue.go
@@ -53,7 +53,7 @@ func init() {
 func NewQueue() schwifty.Box {
 	trackList := tracklist.NewTrackList(
 		tracklist.GroupedColumn(3, gtk.AlignStartValue, tracklist.CoverColumn, tracklist.TitleAlbumColumn),
-		tracklist.ExpandCustomButtonColumn(2, func(_ string, position, _ int) {
+		tracklist.ExpandCustomButtonColumn(1, func(_ string, position, _ int) {
 			go player.SkipThoughQueue(player.UserQueue, position)
 		}),
 		tracklist.GroupedColumn(1, gtk.AlignCenterValue,
