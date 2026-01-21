60e29241f6fc720a189ee00a694183e547e0a54e
NilaTheDragon <nilathedragon@pm.me>
2026-01-21 22:00:24 +0100

    Fix concurrent map writes in schwifty state
    Signed-off-by: NilaTheDragon <nilathedragon@pm.me>

--- pkg/schwifty/state/state.go
+++ pkg/schwifty/state/state.go
@@ -1,20 +1,25 @@
 package state
 
 import (
+	"sync"
+
 	"github.com/google/uuid"
 )
 
 type StateCallback[T any] func(newValue T)
 
 type State[T any] struct {
-	value     T
-	callbacks map[string]StateCallback[T]
-	stateful  bool
+	value         T
+	callbacks     map[string]StateCallback[T]
+	callbacksLock sync.RWMutex
+	stateful      bool
 }
 
 func (s *State[T]) AddCallback(callback StateCallback[T]) string {
 	id := uuid.NewString()
+	s.callbacksLock.Lock()
 	s.callbacks[id] = callback
+	s.callbacksLock.Unlock()
 	if s.stateful {
 		callback(s.value)
 	}
@@ -22,13 +27,17 @@ func (s *State[T]) AddCallback(callback StateCallback[T]) string {
 }
 
 func (s *State[T]) RemoveCallback(id string) {
+	s.callbacksLock.Lock()
 	delete(s.callbacks, id)
+	s.callbacksLock.Unlock()
 }
 
 func (s *State[T]) SetValue(value T) {
+	s.callbacksLock.RLock()
 	for _, callback := range s.callbacks {
 		callback(value)
 	}
+	s.callbacksLock.RUnlock()
 	s.value = value
 }
 
