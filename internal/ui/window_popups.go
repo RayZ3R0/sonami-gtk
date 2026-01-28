8ca9db48fb652400f452c2d79f0d7177acc442c9
drafolin <derg@drafolin.ch>
2026-01-28 17:29:08 +0100

    Add Don't show again checkbox to secret error
    - Add a Don't show again checkbox to the secret service error dialog
    - On Continue, if checked, set hide-secret-service-warning

--- internal/ui/window_popups.go
+++ internal/ui/window_popups.go
@@ -18,13 +18,17 @@ func (w *Window) PresentSecretServiceError(err *secrets.ServiceError) {
 	AlertDialog(err.Title, err.Body).
 		WithCSSClass("no-response").
 		ConnectConstruct(func(ad *adw.AlertDialog) {
+			checkbox := gtk.NewCheckButtonWithLabel(gettext.Get("Don't show again"))
+			checkbox.SetHalign(gtk.AlignBaselineCenterValue)
+			checkbox.AddCssClass("space-2")
+
 			ad.SetExtraChild(
 				VStack(
-					Button().Label(gettext.Get("Continue")).VPadding(10).ConnectClicked(func(b gtk.Button) {
-						ad.Close()
-					}),
-					Button().Label(gettext.Get("Never Show This Again")).WithCSSClass("destructive-action").VPadding(10).ConnectClicked(func(b gtk.Button) {
-						settings.General().SetHideSecretServiceWarning(true)
+					checkbox,
+					Button().Label(gettext.Get("Continue")).WithCSSClass("destructive-action").VPadding(10).ConnectClicked(func(b gtk.Button) {
+						if checkbox.GetActive() {
+							settings.General().SetHideSecretServiceWarning(true)
+						}
 						ad.Close()
 					}),
 				).Spacing(12).ToGTK(),
