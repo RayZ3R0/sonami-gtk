760b19be79f0a73c6f653a2940c64ad3eebc5eef
drafolin <derg@drafolin.ch>
2026-02-04 08:59:19 +0100

    Update link button styles to separate them from the previous category

--- internal/ui/components/dynamic_v1.go
+++ internal/ui/components/dynamic_v1.go
@@ -125,13 +125,21 @@ func ForModule(module v1.Module) schwifty.BaseWidgetable {
 	case v1.ModuleTypePageLinks:
 		list := shortcut_list.NewShortcutList()
 		for _, item := range module.PagedList.Items {
-			list.Append(shortcut_list.NewTextShortcut(item.Title, "").ConnectClicked(func(b gtk.Button) {
-				router.Navigate(strings.ReplaceAll(item.APIPath, "pages/", "explore/"))
-			}))
+			list.Append(
+				shortcut_list.
+					NewTextShortcut(item.Title, "").
+					WithCSSClass("pill").
+					ConnectClicked(func(b gtk.Button) {
+						router.Navigate(strings.ReplaceAll(item.APIPath, "pages/", "explore/"))
+					}),
+			)
 		}
 		return VStack(
-			NewRowTitle().SetTitle(module.Title).HPadding(40),
-			list.HMargin(50),
+			ManagedWidget(&gtk.NewSeparator(gtk.OrientationHorizontalValue).Widget).
+				WithCSSClass("spacer"),
+			Clamp().Child(
+				list.HMargin(50),
+			).MaximumSize(1200),
 		)
 	default:
 		logger.Warn("Unsupported module type", "type", module.Type)
