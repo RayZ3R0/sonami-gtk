1f8c74c24f9223b1a645a62c8e8bbfaaf65e86fa
drafolin <derg@drafolin.ch>
2026-02-08 19:37:09 +0100

    Add artist favourites API and UI support

--- internal/ui/routes/artist.go
+++ internal/ui/routes/artist.go
@@ -3,18 +3,26 @@ package routes
 import (
 	"context"
 	"fmt"
+	"log/slog"
+	"slices"
 
 	"codeberg.org/dergs/tonearm/internal/gettext"
 	"codeberg.org/dergs/tonearm/internal/notifications"
 	"codeberg.org/dergs/tonearm/internal/player"
 	"codeberg.org/dergs/tonearm/internal/resources"
 	"codeberg.org/dergs/tonearm/internal/router"
+	"codeberg.org/dergs/tonearm/internal/secrets"
+	"codeberg.org/dergs/tonearm/internal/signals"
+	appState "codeberg.org/dergs/tonearm/internal/state"
 	"codeberg.org/dergs/tonearm/internal/ui/components"
+	"codeberg.org/dergs/tonearm/pkg/schwifty"
 	. "codeberg.org/dergs/tonearm/pkg/schwifty/syntax"
+	"codeberg.org/dergs/tonearm/pkg/schwifty/tracking"
 	"codeberg.org/dergs/tonearm/pkg/tidalapi"
 	"codeberg.org/dergs/tonearm/pkg/utils/imgutil"
 	"github.com/infinytum/injector"
 	"github.com/jwijenbergh/puregotk/v4/gdk"
+	"github.com/jwijenbergh/puregotk/v4/gobject"
 	"github.com/jwijenbergh/puregotk/v4/gtk"
 	"github.com/jwijenbergh/puregotk/v4/pango"
 )
@@ -22,6 +30,8 @@ import (
 func init() {
 	router.Register("artist/:id", Artist)
 }
+
+var artistLogger = slog.With("module", "ui/routes", "route", "artist")
 
 func Artist(artistId string) *router.Response {
 	tidal := injector.MustInject[*tidalapi.TidalAPI]()
@@ -35,6 +45,8 @@ func Artist(artistId string) *router.Response {
 	for _, item := range artistPage.Items {
 		body = body.Append(components.ForPageItem(item))
 	}
+
+	isArtistFavourited := signals.NewStatefulSignal(false)
 
 	return &router.Response{
 		PageTitle: gettext.Get("Artist"),
@@ -107,8 +119,61 @@ func Artist(artistId string) *router.Response {
 							TooltipText(gettext.Get("Add to Collection")).
 							IconName("heart-outline-thick-symbolic").
 							WithCSSClass("flat").
-							Sensitive(false),
-						Button().
+							ConnectConstruct(func(b *gtk.Button) {
+								favLists, err := appState.Favourites()
+								favList := favLists.Artist
+								if err != nil {
+									albumLogger.Error("Failed to load favourites", err)
+									b.SetIconName("heart-outline-thick-symbolic")
+									b.RemoveCssClass("accent")
+
+									return
+								}
+
+								isArtistFavourited.Notify(func(oldValue bool) bool {
+									return slices.Contains(favList, artistId)
+								})
+
+								weakRef := tracking.NewWeakRef(&b.Object)
+								isArtistFavourited.On(func(value bool) bool {
+									schwifty.OnMainThreadOncePure(func() {
+										weakRef.Use(func(obj *gobject.Object) {
+											b := gtk.ButtonNewFromInternalPtr(obj.Ptr)
+
+											if value {
+												b.SetIconName("heart-filled-symbolic")
+												b.AddCssClass("accent")
+											} else {
+												b.SetIconName("heart-outline-thick-symbolic")
+												b.RemoveCssClass("accent")
+											}
+										})
+									})
+
+									return signals.Continue
+								})
+							}).
+							ConnectClicked(func(b gtk.Button) {
+								tidal, _ := injector.Inject[*tidalapi.TidalAPI]()
+
+								isArtistFavourited.Notify(func(oldValue bool) bool {
+									if oldValue {
+										err := tidal.V1.Favourites.RemoveArtist(context.Background(), secrets.UserID(), artistId)
+										if err != nil {
+											artistLogger.Error("error while removing artist from favourites", "error", err)
+											return oldValue
+										}
+									} else {
+										err := tidal.V1.Favourites.AddArtist(context.Background(), secrets.UserID(), artistId)
+										if err != nil {
+											artistLogger.Error("error while adding artist to favourites", "error", err)
+											return oldValue
+										}
+									}
+
+									return !oldValue
+								})
+							}), Button().
 							TooltipText(gettext.Get("Copy Artist URL")).
 							IconName("share-alt-symbolic").
 							WithCSSClass("flat").
