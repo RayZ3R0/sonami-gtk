7e14d82717765169b3955bf1ce5f2babdeba8504
drafolin <derg@drafolin.ch>
2026-02-08 18:21:36 +0100

    Add album favourites support and API
    Wire the album page to reflect and toggle favourite state.
    Add Album favourites management with AddAlbum and RemoveAlbum methods
    to the Tidal API client.

--- internal/ui/routes/album.go
+++ internal/ui/routes/album.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"context"
 	"fmt"
+	"log/slog"
+	"slices"
 	"strings"
 
 	"codeberg.org/dergs/tonearm/internal/gettext"
@@ -10,21 +12,26 @@ import (
 	"codeberg.org/dergs/tonearm/internal/player"
 	"codeberg.org/dergs/tonearm/internal/resources"
 	"codeberg.org/dergs/tonearm/internal/router"
+	"codeberg.org/dergs/tonearm/internal/secrets"
 	"codeberg.org/dergs/tonearm/internal/signals"
+	appState "codeberg.org/dergs/tonearm/internal/state"
 	"codeberg.org/dergs/tonearm/internal/ui/components/tracklist"
 	"codeberg.org/dergs/tonearm/internal/ui/pages"
 	"codeberg.org/dergs/tonearm/pkg/schwifty"
 	"codeberg.org/dergs/tonearm/pkg/schwifty/state"
 	. "codeberg.org/dergs/tonearm/pkg/schwifty/syntax"
+	"codeberg.org/dergs/tonearm/pkg/schwifty/tracking"
 	"codeberg.org/dergs/tonearm/pkg/tidalapi"
 	"codeberg.org/dergs/tonearm/pkg/tidalapi/models/openapi"
 	"codeberg.org/dergs/tonearm/pkg/tidalapi/pagination"
 	"codeberg.org/dergs/tonearm/pkg/utils/imgutil"
 	"github.com/infinytum/injector"
 	"github.com/jwijenbergh/puregotk/v4/gdk"
+	"github.com/jwijenbergh/puregotk/v4/gobject"
 	"github.com/jwijenbergh/puregotk/v4/gtk"
 )
 
+var albumLogger = slog.With("module", "ui/routes", "route", "album")
 var canPlayAlbumState = state.NewStateful(false)
 
 func init() {
@@ -75,6 +82,8 @@ func Album(albumId string) *router.Response {
 			return tl.HMargin(30).VAlign(gtk.AlignStartValue)
 		},
 	)
+
+	isAlbumFavourited := signals.NewStatefulSignal(false)
 
 	return &router.Response{
 		PageTitle: album.Data.Attributes.Title,
@@ -146,7 +155,62 @@ func Album(albumId string) *router.Response {
 						Button().
 							TooltipText(gettext.Get("Add to Collection")).
 							IconName("heart-outline-thick-symbolic").
-							WithCSSClass("flat").Sensitive(false),
+							WithCSSClass("flat").
+							ConnectConstruct(func(b *gtk.Button) {
+								favLists, err := appState.Favourites()
+								favList := favLists.Album
+								if err != nil {
+									albumLogger.Error("Failed to load favourites", err)
+									b.SetIconName("heart-outline-thick-symbolic")
+									b.RemoveCssClass("accent")
+
+									return
+								}
+
+								isAlbumFavourited.Notify(func(oldValue bool) bool {
+									return slices.Contains(favList, albumId)
+								})
+
+								weakRef := tracking.NewWeakRef(&b.Object)
+								isAlbumFavourited.On(func(value bool) bool {
+									schwifty.OnMainThreadOncePure(func() {
+										weakRef.Use(func(obj *gobject.Object) {
+											b := gtk.ButtonNewFromInternalPtr(obj.Ptr)
+
+											if value {
+												b.SetIconName("heart-filled-symbolic")
+												b.AddCssClass("accent")
+											} else {
+												b.SetIconName("heart-outline-thick-symbolic")
+												b.RemoveCssClass("accent")
+											}
+										})
+									})
+
+									return signals.Continue
+								})
+							}).
+							ConnectClicked(func(b gtk.Button) {
+								tidal, _ := injector.Inject[*tidalapi.TidalAPI]()
+
+								isAlbumFavourited.Notify(func(oldValue bool) bool {
+									if oldValue {
+										err := tidal.V1.Favourites.RemoveAlbum(context.Background(), secrets.UserID(), albumId)
+										if err != nil {
+											albumLogger.Error("error while removing album from favourites", "error", err)
+											return oldValue
+										}
+									} else {
+										err := tidal.V1.Favourites.AddAlbum(context.Background(), secrets.UserID(), albumId)
+										if err != nil {
+											albumLogger.Error("error while adding album to favourites", "error", err)
+											return oldValue
+										}
+									}
+
+									return !oldValue
+								})
+							}),
 						Button().
 							TooltipText(gettext.Get("Copy Album URL")).
 							IconName("share-alt-symbolic").
