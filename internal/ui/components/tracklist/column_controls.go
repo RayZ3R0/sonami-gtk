67678e61c8840d193e946928ce2fbc906af3ff26
NilaTheDragon <nilathedragon@pm.me>
2026-02-19 01:47:17 +0100

    Fix Column Controls never unreffing any of the menus

--- internal/ui/components/tracklist/column_controls.go
+++ internal/ui/components/tracklist/column_controls.go
@@ -9,6 +9,7 @@ import (
 	"codeberg.org/dergs/tonearm/internal/state"
 	favouritebutton "codeberg.org/dergs/tonearm/internal/ui/components/favourite_button"
 	. "codeberg.org/dergs/tonearm/pkg/schwifty/syntax"
+	"codeberg.org/dergs/tonearm/pkg/schwifty/tracking"
 	"codeberg.org/dergs/tonearm/pkg/tonearm"
 	"github.com/jwijenbergh/puregotk/v4/gio"
 	"github.com/jwijenbergh/puregotk/v4/glib"
@@ -24,26 +25,32 @@ type lightArtist struct {
 
 func controlsColumn(trackId, albumId string, artistId []lightArtist, grid *gtk.Grid, position int, column int) int {
 	model := gio.NewMenu()
+	tracking.SetFinalizer("Menu", model)
 
 	item := gio.NewMenuItem(gettext.Get("Navigate to Album"), "win.route.album")
 	item.SetActionAndTargetValue("win.route.album", glib.NewVariantString(albumId))
+	tracking.SetFinalizer("MenuItem", item)
 	model.AppendItem(item)
 
 	if len(artistId) > 1 {
 		submenu := gio.NewMenu()
+		tracking.SetFinalizer("Menu", submenu)
 		for _, artist := range artistId {
 			item := gio.NewMenuItem(fmt.Sprintf(gettext.Get("Navigate to %s"), artist.Name), "win.route.artist")
 			item.SetActionAndTargetValue("win.route.artist", glib.NewVariantString(artist.ID))
+			tracking.SetFinalizer("MenuItem", item)
 			submenu.AppendItem(item)
 		}
 		model.AppendSubmenu(gettext.Get("Navigate to Artist"), &submenu.MenuModel)
 	} else if len(artistId) == 1 {
 		item := gio.NewMenuItem(gettext.Get("Navigate to Artist"), "win.route.artist")
 		item.SetActionAndTargetValue("win.route.artist", glib.NewVariantString(artistId[0].ID))
+		tracking.SetFinalizer("MenuItem", item)
 		model.AppendItem(item)
 	}
 
 	popover := gtk.NewPopoverMenuFromModel(&model.MenuModel)
+	tracking.SetFinalizer("PopoverMenu", popover)
 
 	grid.Attach(
 		HStack(
