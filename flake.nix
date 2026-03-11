655ab35233193b48c55a0affc7cedf547a50a1fb
RayZ3R0 <iamrayyanahsan@gmail.com>
2026-03-12 00:37:57 +0600

    chore: release v1.5.6

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
 
         packages.tonearm = pkgs.buildGoModule.override { go = pkgs.go_1_26; } (finalAttrs: {
           pname = "sonami";
-          version = "1.5.5";
+          version = "1.5.6";
           src = pkgs.lib.cleanSource ./.;
           vendorHash = "sha256-jwv80SkHVPqsWdIsVyFEw1J+8kOpg38gObYgtxnlv6o=";
 
